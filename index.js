563861246fba0eb3a7743357a4ae516fa0f08c22
Senurcreate <shr1212ni@gmail.com>
2026-06-27 06:55:13 +0530

    Import packages

---
+++ index.js
@@ -0,0 +1,16 @@
+import pkg from '@slack/bolt';
+const {App} = pkg;
+import { WebClient } from '@slack/web-api';
+import { ChatOpenAI } from '@langchain/openai';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import express from 'express';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const log = {
+    info: (msg, ...args) => console.log(`[INFO] ${msg}`, ...args),
+    error: (msg, ...args) => console.log(`[ERROR] ${msg}`, ...args),
+    debug: (msg, ...args) => process.env.NODE_ENV === "development" && console.log(`[DEBUG] ${msg}`, ...args)
+}
